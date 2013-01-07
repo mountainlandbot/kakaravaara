916b4a807c55f52dc3b089c6da9425e603b2625c
Jason Robinson <jaywink@basshero.org>
2013-01-07 12:26:15 +0200

    If local settings not found - import prod settings

--- settings.py
+++ settings.py
@@ -432,7 +432,10 @@ DEBUG_TOOLBAR_CONFIG = {"INTERCEPT_REDIRECTS": False}
 try:
     from local_settings import *
 except ImportError:
-    pass
+    try:
+        from prod_settings import *
+    except ImportError:
+        pass
 
 
 ####################
