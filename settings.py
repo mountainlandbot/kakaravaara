4f3be9d55d6d385c3fcfd34cd0587ca1abf30ea0
Jason Robinson <mail@jasonrobinson.me>
2014-02-26 23:55:40 +0200

    Add header image. Some layouting

--- settings.py
+++ settings.py
@@ -288,6 +288,10 @@ ROOT_URLCONF = "%s.urls" % PROJECT_DIRNAME
 # Don't forget to use absolute paths, not relative paths.
 TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, "templates"),)
 
+STATICFILES_DIRS = (
+    "staticfiles/",    
+)
+
 
 ################
 # APPLICATIONS #
